c47f60caa096bed30e8aab543d209f9fb213658f
amangupta37 <amangupta6121998@gmail.com>
2021-06-24 08:32:37 +0530

    improved : Write Post UI

--- src/components/Writepost.js
+++ src/components/Writepost.js
@@ -7,17 +7,28 @@ const Writepost = (props) => {
   return (
     <Container>
       <PostContainer>
-        <Postheader canclePostpopupo={props.discardPost} />
-        <TextContainer>
-          <Userprofile />
-          <TextAreaContainer>
-            <textarea placeholder="What do you want to talk about ?"></textarea>
-          </TextAreaContainer>
-        </TextContainer>
-        <Media />
-        <PostItemsContainer>
-          <button>Post</button>
-        </PostItemsContainer>
+        <HeaderContainer>
+          <Postheader canclePostpopupo={props.discardPost} />
+        </HeaderContainer>
+        <form>
+          <FormContainer>
+            <TextContainer>
+              <Userprofile />
+              <TextAreaContainer>
+                <textarea
+                  type="text"
+                  placeholder="What do you want to talk about ?"
+                />
+              </TextAreaContainer>
+            </TextContainer>
+            <MediaContainer>
+              <Media />
+              <PostItemsContainer>
+                <button>Post</button>
+              </PostItemsContainer>
+            </MediaContainer>
+          </FormContainer>
+        </form>
       </PostContainer>
     </Container>
   );
@@ -27,18 +38,16 @@ export default Writepost;
 
 const Container = styled.div`
   width: 100%;
-  display: grid;
-  place-items: center;
-  padding-top: 5rem;
+  height: 100vh;
   position: fixed;
-  @media only screen and (max-width: 600px) {
-    padding-top: 2rem;
-  }
+  background: rgba(0, 0, 0, 0.8);
 `;
 
 const PostContainer = styled.div`
   width: 35%;
-  height: auto;
+  margin: 0 auto;
+  margin-top: 5rem;
+  // height: 400px;
   background-color: #fff;
   border-radius: 5px;
   position: relative;
@@ -46,28 +55,42 @@ const PostContainer = styled.div`
   box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
   padding: 12px;
   display: grid;
-  grid-template-rows: 50px auto 280px;
+  grid-template-rows: 50px 1fr;
   @media only screen and (max-width: 600px) {
-    width: 90%;
-    grid-template-rows: 50px auto 200px;
+    width: 95%;
+    margin-top: 3rem;
   }
+`;
+const HeaderContainer = styled.div`
+  width: 100%;
+  // background: lime;
+`;
+
+const FormContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  // background: blue;
+  display: grid;
+  grid-template-rows: 200px 1fr;
 `;
 
 const TextContainer = styled.div`
   width: 100%;
   border-top: 2px solid #3333;
   display: grid;
-  grid-template-rows: 90px 1fr;
+  grid-template-rows: 80px 1fr;
+  // background: red;
 `;
 
 const TextAreaContainer = styled.div`
   width: 100%;
-  background: red;
+  height: 100%;
   textarea {
     width: 100%;
     height: 100%;
     outline: none;
     border: none;
+    resize: none;
     padding-left: 0.7rem;
     font-size: 0.9rem;
     font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI",
@@ -79,17 +102,26 @@ const TextAreaContainer = styled.div`
 
 const PostItemsContainer = styled.div`
   width: 100%;
-  border-left: 2px solid #3333;
+  // border-left: 2px solid #3333;
   padding-right: 1rem;
   display: flex;
   flex-direction: row-reverse;
+
+  // background: yellow;
   button {
     width: 100px;
-    height: 2.5rem;
+    height: 2rem;
     cursor: pointer;
     border-radius: 2rem;
     border: none;
     outline: none;
     background: #3333;
   }
+`;
+
+const MediaContainer = styled.div`
+  width: 100%;
+  // background: green;
+  display: flex;
+  align-items: flex-end;
 `;
